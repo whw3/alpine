2e8ad59010a25f3925cb65b184063321f715494d
whw3 <whw3@users.noreply.github.com>
2017-07-13 11:56:14 -0500

    added checksum and GPG signature verification

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM scratch
-ADD alpine-minirootfs-3.6.1-armhf.tar.gz /
-COPY .* /root/
-RUN apk --no-cache add s6 bash bash-completion nano
--- build.sh
+++ build.sh
@@ -24,4 +24,32 @@ EOF
 [[ ! -f alpine-minirootfs-$ALPINE_VERSION-armhf.tar.gz ]] && \
   wget https://nl.alpinelinux.org/alpine/v$RELEASE/releases/armhf/alpine-minirootfs-$ALPINE_VERSION-armhf.tar.gz
 
+[[ ! -f alpine-minirootfs-$ALPINE_VERSION-armhf.tar.gz.sha256 ]] && \
+  wget https://nl.alpinelinux.org/alpine/v$RELEASE/releases/armhf/alpine-minirootfs-$ALPINE_VERSION-armhf.tar.gz.sha256
+ 
+[[ ! -f alpine-minirootfs-$ALPINE_VERSION-armhf.tar.gz.asc ]] && \
+  wget https://nl.alpinelinux.org/alpine/v$RELEASE/releases/armhf/alpine-minirootfs-$ALPINE_VERSION-armhf.tar.gz.asc
+
+if [[  $(sha256sum -c  alpine-minirootfs-$ALPINE_VERSION-armhf.tar.gz.sha256) ]] ; then
+    echo "Checksum: OK"
+else
+    echo "Checksum: INVALID...Build Terminated"
+    exit 2
+fi
+if [[ ! $(gpg --fingerprint 293ACD0907D9495A 2>/dev/null) ]] ; then 
+    echo "Importing NCOPA key"
+    gpg --import ncopa.asc
+fi
+
+NCOPA_FPR=$( gpg --with-colons --fingerprint 293ACD0907D9495A 2>/dev/null | grep fpr | cut -d ':' -f 10 )
+gpg --verify alpine-minirootfs-$ALPINE_VERSION-armhf.tar.gz.asc alpine-minirootfs-$ALPINE_VERSION-armhf.tar.gz  &>SIGNATURE
+FPR=$(grep fingerprint SIGNATURE|sed 's/Primary key fingerprint: //;s/ //g')
+rm SIGNATURE
+if [[ "$FPR" = "$NCOPA_FPR" ]]; then
+    echo "Signature: OK"
+else
+    echo "Signature: Failed Valitdation...Build Terminated"
+    exit 2
+fi
+echo "Building..."
 docker build -t whw3/alpine .
