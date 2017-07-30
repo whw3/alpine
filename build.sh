41192b8f597505ebd44acee6668a835cac44e105
whw3 <whw3@users.noreply.github.com>
2017-07-30 15:20:26 -0500

    automated download of latest tarball

--- build.sh
+++ build.sh
@@ -1,17 +1,23 @@
 #/bin/bash
-#ALPINE_VERSION="3.5.2"
-#RELEASE="3.5"
+if  [[ "$(which jq)" = "" ]]; then
+	apt-get update
+	apt-get install -y jq
+fi
 S6_VERSION="v1.19.1.1"
-ALPINE_VERSION="3.6.2"
-RELEASE="3.6"
+TZ="America/Chicago"
+[[ -f latest-releases.yaml ]] && rm latest-releases.yaml
+wget https://nl.alpinelinux.org/alpine/latest-stable/releases/armhf/latest-releases.yaml
+ALPINE_VERSION=$(./yaml2json.py < latest-releases.yaml | jq '.[]|select(.flavor == "alpine-minirootfs")|.version'| sed 's/\"//g')
+ALPINE_BRANCH=$(./yaml2json.py < latest-releases.yaml | jq '.[]|select(.flavor == "alpine-minirootfs")|.branch'| sed 's/\"//g')
+RELEASE=$( echo $ALPINE_BRANCH|sed 's/^v//')
 MIRROR="http://dl-cdn.alpinelinux.org/alpine"
 PACKAGES="alpine-baselayout,alpine-keys,apk-tools,libc-utils"
 cat << EOF > options 
-export RELEASE="v$RELEASE"
+export RELEASE="$ALPINE_BRANCH"
 export MIRROR="$MIRROR"
 export PACKAGES="$PACKAGES"
-export BUILD_OPTIONS=(-b -s -t UTC -r v$RELEASE -m $MIRROR -p $PACKAGES)
-export TAGS=(whw3/alpine:$RELEASE whw3/alpine:latest)
+export BUILD_OPTIONS=(-b -s -t UTC -r $ALPINE_BRANCH -m $MIRROR -p $PACKAGES)
+export TAGS=(whw3/alpine:$ALPINE_VERSION whw3/alpine:$RELEASE whw3/alpine:latest)
 EOF
 
 cat << EOF > Dockerfile
@@ -21,8 +27,8 @@ ADD s6-overlay-$S6_VERSION-armhf.tar.gz /
 COPY .* /root/
 RUN apk --no-cache add bash bash-completion nano git 
 RUN apk --no-cache add tzdata ; \
-cp /usr/share/zoneinfo/America/Chicago /etc/localtime; \
-echo "America/Chicago" > /etc/timezone; \
+cp /usr/share/zoneinfo/$TZ /etc/localtime; \
+echo "$TZ" > /etc/timezone; \
 apk del tzdata
 ENTRYPOINT ["/init"]
 EOF
@@ -31,13 +37,13 @@ EOF
     wget -O s6-overlay-$S6_VERSION-armhf.tar.gz https://github.com/just-containers/s6-overlay/releases/download/$S6_VERSION/s6-overlay-armhf.tar.gz
 
 [[ ! -f alpine-minirootfs-$ALPINE_VERSION-armhf.tar.gz ]] && \
-  wget https://nl.alpinelinux.org/alpine/v$RELEASE/releases/armhf/alpine-minirootfs-$ALPINE_VERSION-armhf.tar.gz
+  wget https://nl.alpinelinux.org/alpine/$ALPINE_BRANCH/releases/armhf/alpine-minirootfs-$ALPINE_VERSION-armhf.tar.gz
 
 [[ ! -f alpine-minirootfs-$ALPINE_VERSION-armhf.tar.gz.sha256 ]] && \
-  wget https://nl.alpinelinux.org/alpine/v$RELEASE/releases/armhf/alpine-minirootfs-$ALPINE_VERSION-armhf.tar.gz.sha256
+  wget https://nl.alpinelinux.org/alpine/$ALPINE_BRANCH/releases/armhf/alpine-minirootfs-$ALPINE_VERSION-armhf.tar.gz.sha256
  
 [[ ! -f alpine-minirootfs-$ALPINE_VERSION-armhf.tar.gz.asc ]] && \
-  wget https://nl.alpinelinux.org/alpine/v$RELEASE/releases/armhf/alpine-minirootfs-$ALPINE_VERSION-armhf.tar.gz.asc
+  wget https://nl.alpinelinux.org/alpine/$ALPINE_BRANCH/releases/armhf/alpine-minirootfs-$ALPINE_VERSION-armhf.tar.gz.asc
 
 if [[  $(sha256sum -c  alpine-minirootfs-$ALPINE_VERSION-armhf.tar.gz.sha256) ]] ; then
     echo "Checksum: OK"
@@ -62,3 +68,5 @@ else
 fi
 echo "Building..."
 docker build -t whw3/alpine .
+docker tag whw3/alpine whw3/alpine:$RELEASE
+docker tag whw3/alpine whw3/alpine:$ALPINE_VERSION
