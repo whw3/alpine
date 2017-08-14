870f5b45e6d1e0cdbecf6e43d30459a3fbf0567e
whw3 <whw3@users.noreply.github.com>
2017-08-14 09:52:49 -0500

    s6 docker-entrypoint

--- build.sh
+++ build.sh
@@ -47,6 +47,7 @@ FROM scratch
 ADD alpine-minirootfs-$ALPINE_VERSION-armhf.tar.gz /
 ADD s6-overlay-$S6_VERSION-armhf.tar.gz /
 COPY root /root/
+COPY 01-docker-entrypoint.sh /etc/cont-init.d
 RUN apk --no-cache add bash bash-completion nano git tzdata
 RUN chmod 0700 /root/bin/tzconfig && echo "$TZ" > /etc/timezone; cp /usr/share/zoneinfo/$TZ /etc/localtime && exit 0 ; exit 1
 RUN apk del tzdata
